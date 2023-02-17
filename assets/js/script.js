bc07d863f5d1f1dbebb91b0ba0e3aa3b4b9c985c
MariaHoulihan <mariahoulihan@gmail.com>
2023-02-17 17:00:39 +0000

    Add id to quiz questions. Add next button and style for next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let questions = [
-    {
+    { 
+      id: '0',
       question: 'The occipital area of the skull is located in what part of the skull?',
       option1: 'Front',
       option2: 'Back',
@@ -7,6 +8,7 @@ let questions = [
       answer: 'Back',
     },
     {
+      id: '1',
       question: 'The biceps femoris muscle is located in what part of the body?',
       option1: 'Shoulder', 
       option2: 'Arm',
@@ -14,6 +16,7 @@ let questions = [
       answer: 'Leg',
     },
     {
+      id: '2',
       question: 'The elbow is an example of what type of joint?',
       option1: 'Pivot joint',
       option2: 'Hinge joint',
@@ -21,6 +24,7 @@ let questions = [
       answer: 'Hinge joint',
     },
      { 
+      id: '3',
       question: 'The muscles located at the front of the thigh are known as what?',
       option1: 'Adductors', 
       option2: 'Hamstrings', 
@@ -28,6 +32,7 @@ let questions = [
       answer: 'Quadriceps',
      },
      {
+      id: '4',
       question: 'The C1 vertebra is called?',
       option1: 'Atlas',
       option2: 'Axis',
@@ -35,6 +40,7 @@ let questions = [
       answer: 'Atlas'
      }, 
      {
+      id: '5',
       question: 'The three bones that form the ankle are the fibula, tibia and which other bone?',
       option1: 'Talus',
       option2: 'Calcaneous',
@@ -42,27 +48,31 @@ let questions = [
       answer: 'Talus',
      },
      {
+      id: '6',
       question: 'Which vessel returns unoxygenated blood to the lungs?',
       option1: 'Aorta',
       option2: 'Pulmonary artery',
       option3: 'Pulmonary vein',
       answer: 'Pulmonary artery'
      }, 
-      {
+    {
+      id: '7',
       question: 'Ligaments and Tendons are composed of what type of tissue?',
       option1: 'Epithelial',
       option2: 'Adipose',
       option3: 'Connective',
       answer: 'Connective',
       },
-       {
+    {
+      id: '8',
       question: 'The anatomical term for the head is?',
       option1: 'Cephalic',
       option2: 'Cervical',
       option3: 'Thoracic',
       answer: 'Cephalic',
        }, 
-        {
+    {
+      id: '9',
       question: 'Mastication is the term for what?',
       option1: 'Chewing',
       option2: 'Snoring',
@@ -71,4 +81,10 @@ let questions = [
         }  
       ];
 
-      
+      let questionContainer = document.getElementById('question-area');
+      let startButton = document.getElementById('start-button');
+      let nextButton = document.getElementById('next-q');
+
+      console.log(questionContainer);
+      console.log(questions);
+
